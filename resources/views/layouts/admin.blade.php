5387967e69525757dd7154fe5c3ebe8be5cc6e6a
thuongx1bg <thuongx1bg@gmail.com>
2021-09-29 22:18:31 +0700

    Update admin.blade.php

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -15,6 +15,7 @@ scratch. This page gets rid of all links and provides the needed markup only.
   <link rel="stylesheet" href="{{asset('adminLTE/plugins/fontawesome-free/css/all.css')}}">
   <!-- Theme style -->
   <link rel="stylesheet" href="{{asset('adminLTE/dist/css/adminlte.min.css')}}">
+  @yield('css')
 </head>
 <body class="hold-transition sidebar-mini">
 <div class="wrapper">
@@ -40,5 +41,6 @@ scratch. This page gets rid of all links and provides the needed markup only.
 <script src="{{asset('adminLTE/plugins/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
 <!-- AdminLTE App -->
 <script src="{{asset('adminLTE/dist/js/adminlte.min.js')}}"></script>
+@yield('js')
 </body>
 </html>
