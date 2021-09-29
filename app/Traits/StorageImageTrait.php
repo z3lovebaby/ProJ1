35651b6d2b3630d15bfb357a54d98870ad8013eb
thuongx1bg <thuongx1bg@gmail.com>
2021-09-29 22:17:56 +0700

    Create StorageImageTrait.php

---
+++ app/Traits/StorageImageTrait.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Traits;
+
+use Illuminate\Support\Str;
+
+use Illuminate\Support\Facades\Storage;
+
+
+trait StorageImageTrait
+{
+    public function storageTraitUploat($request, $fieldName, $foderName)
+    {
+        if ($request->hasFile($fieldName)) {
+            $file = $request->$fieldName;
+            $fileNameOrigin = $file->getClientOriginalName();
+            $fileNameHash = Str::random(20) . '.' . $file->getClientOriginalName();
+
+            $filePath = $request->file($fieldName)->storeAs('public/' . $foderName . '/' . auth()->id(), $fileNameHash);
+            $dataUploadTrait = [
+                'file_name' => $fileNameOrigin,
+                'file_path' => Storage::url($filePath)
+            ];
+            return $dataUploadTrait;
+        }
+        return null;
+    }
+
+    
+}
