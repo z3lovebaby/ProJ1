a859731b842480f04fda9c2956595c614045acab
thuongx1bg <thuongx1bg@gmail.com>
2021-10-02 21:02:15 +0700

    Create RequestAdvert.php

---
+++ app/Http/Requests/RequestAdvert.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class RequestAdvert extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'name'=>'required',
+            'description'=>'required',
+            'image_path'=>'required',
+        ];
+    }
+}
