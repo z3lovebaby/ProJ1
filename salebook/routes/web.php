2b0fd455dd6012ca60713289f9bba76166a7fbe0
TrangNgan248 <trangnganvirgo@gmail.com>
2021-10-08 21:08:02 +0700

    add Meu bar

--- salebook/routes/web.php
+++ salebook/routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 //use App\Http\Controllers\HomeController;
 
@@ -15,5 +16,5 @@ use Illuminate\Support\Facades\Route;
 */
 //Route::get('/', [HomeController::class, 'index']);
 
-Route::get('/', action: 'HomeController@index');
+Route::get('/', action: 'HomeController@index')->name('home');
 
