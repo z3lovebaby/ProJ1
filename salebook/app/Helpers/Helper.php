434c59a7a84243f5e7901127a5011565142fe94a
TrangNgan248 <trangnganvirgo@gmail.com>
2021-10-10 03:06:24 +0700

    delete front end

--- salebook/app/Helpers/Helper.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-function getConfigValueFromSettingTable($configKey){
-    $setting = App\Models\Setting::where('config_key', $configKey)->first();
-    if (!empty($setting)){
-        return  $setting->config_value;
-    }
-    return null;
-}
--- salebook/resources/views/test.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('layouts.master')
-
-@section('title')
-    <title>Test page</title>
-@endsection
-
-@section('content')
-    <div>ABC</div>
-@endsection
